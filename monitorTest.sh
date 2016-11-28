0e27b2be00e3018988fe66571e482ef1707dff81
Isabello <isabella@lisk.io>
2016-11-28 17:35:14 -0600

    Add easy to use scripts for failover monitor

---
+++ monitorTest.sh
@@ -0,0 +1,8 @@
+HOST1=""
+HOST2=""
+HOST3=""
+HOST4=""
+CONFIG="src/liskak_testnet.json"
+LOG_FILE="logs/forgingTest.log"
+pkill -f $CONFIG -9
+nohup bash liskak.sh -c $CONFIG -f $HOST1 $HOST2 $HOST3 $HOST4 > $LOG_FILE 2>&1&
