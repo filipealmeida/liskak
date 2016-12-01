53c0f5196a257ad84db52f30e33692774eaa3eea
Filipe de Almeida <filipe@marmelos.com>
2016-12-01 20:08:14 +0000

    List/Grab latest blockchain snapshots

---
+++ tools/snapshot.newestURL.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+SOURCES="\
+ https://snapshot.liskwallet.net/blockchain.db.gz\
+ https://snapshot.lisknode.io/blockchain.db.gz\
+ https://lisktools.io/backups/blockchain.db.gz\
+"
+
+for source in ${SOURCES}; do 
+  LMHEAD=`curl -m 5 -svI ${source} 2>&1 | grep '^< Last-Modified:'|sed 's/< Last-Modified://'`
+  DATESTR=`date "+%Y%m%d%H%M%S%z" --date="${LMHEAD}"`
+  echo ${DATESTR} ${source} ${LMHEAD}
+done | sort | tail -1 | awk {'print $2'}
