80e2da603945a85655d2878ae8e365b75e774aa1
Filipe de Almeida <filipe@marmelos.com>
2016-12-03 19:25:24 +0000

    Added extra blockchain sources to tools

--- tools/snapshot.newestURL.sh
+++ tools/snapshot.newestURL.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 
 SOURCES="\
+ https://downloads.lisk.io/lisk/main/blockchain.db.gz\
  https://snapshot.liskwallet.net/blockchain.db.gz\
  https://snapshot.lisknode.io/blockchain.db.gz\
  https://lisktools.io/backups/blockchain.db.gz\
+ https://snapshot.punkrock.me/blockchain.db.gz\
+ https://snapshot.lsknode.org/blockchain.db.gz\
 "
 
 for source in ${SOURCES}; do 
